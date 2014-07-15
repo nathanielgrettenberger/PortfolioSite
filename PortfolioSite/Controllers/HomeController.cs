ba0691bfe361f47c44adcdae9b02263b372f4a08
Nathaniel Grettenberger <n.grettenberger@aol.com>
2014-07-15 11:13:45 -0600

    My website add in

---
+++ PortfolioSite/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Get Home/about
+        public ActionResult About()
+        {
+            return View();
+        }
+        // Get: Home/Work/
+        public ActionResult Work()
+        {
+            return View();
+        }
+        // Get: Home/ Contact
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            Models.ContactsForm cf = new Models.ContactsForm();
+            return View(cf);
+                
+        
+        }
+        // post home contacts
+        [HttpPost]
+        public ActionResult Contact(Models.ContactsForm cf)
+    {
+        Models.NathanielEntities db = new Models.NathanielEntities();
+        db.ContactsForms.Add(cf);
+        db.SaveChanges();
+        return RedirectToAction("ThankYou");
+
+    }
+        // redirect usre to thank you page
+        public ActionResult ThankYou()
+    {
+        return View();
+    }
+        // my addin of link My Bio
+        public ActionResult MyBio()
+        {
+            return View();
+        
+        }
+    }
+
+}
---
+++ PortfolioSite/Models/contactformvalidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioSite.Models
+{
+    [MetadataType(typeof(ContactFormValidation))]
+    public partial class ContactsForm
+    { 
+    
+    }
+    public class ContactFormValidation
+    {
+        // validtae the name
+        [Required, Display(Name= "Your Name")]
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        [Required, EmailAddress(ErrorMessage= " Your email needs to be something@something.com ")]
+        public string Email { get; set; }
+        // validate the email
+        [Required, DataType(DataType.MultilineText)]
+        public string Comments { get; set; }
+       
+        
+    }
+}
